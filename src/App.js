f9aca4cf4fab538d259d3e8e4788c718cf263299
AtishaySawarna <94068168+AtishaySawarna@users.noreply.github.com>
2022-06-26 16:23:04 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import Comments from "./comments/Comments";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Card
+        img="https://images.unsplash.com/photo-1583695477819-357b45d15825?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZnJvbnRlbmQlMjBwcm9qZWN0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+        title="Article-1"
+        description="HTML/CSS"
+        
+      />
+
+      <Card
+        img ="https://images.unsplash.com/photo-1593720218365-b2076cfdefee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8ZnJvbnRlbmQlMjBwcm9qZWN0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+        title="Article-2"
+        description="CSS/BOOTSTRAP"
+      />
+
+      <Card
+        img="https://images.unsplash.com/photo-1593720218746-e13e4a422a3b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGZyb250ZW5kJTIwcHJvamVjdHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+        title="Article-3"
+        description="REACT"
+      />
     </div>
   );
 }
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="card__body">
+        <img src={props.img} class="card__image" />
+        <h2 className="card__title">{props.title}</h2>
+        <p className="card__description">{props.description}</p>
+      </div>
+      <Comments
+        commentsUrl="http://localhost:3000/comments"
+        currentUserId="1"
+      />
+    </div>
+  );
+}
+  
 
 export default App;
